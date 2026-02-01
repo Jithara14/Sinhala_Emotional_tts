3c9a5b6483b3b384a04239312b342a44ccfbb399
Jithara14 <it22336590@my.sliit.lk>
2026-02-01 18:21:34 +0530

    Remove label2 file

--- modules/sinta_emotional_tts.py
+++ modules/sinta_emotional_tts.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:359defbc01e5e22771648906bc47755b7a0340f62780a9af69a21ac104b58cfb
-size 10070
+oid sha256:82a69f351d90f0d5b8c08a39aba418d90a9d456c4481d11012dcfff46b887445
+size 9996
